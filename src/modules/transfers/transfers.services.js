b374cf4a0137c5dd4e11ad7b33354627ee40287d
Arturo <artcrash7@gmail.com>
2023-12-11 15:20:50 -0600

    first commit

---
+++ src/modules/transfers/transfers.services.js
@@ -0,0 +1,15 @@
+import { Transfers } from './transfers.model.js'
+
+export class TransferService {
+  static async create(data) {
+    return await Transfers.create(data)
+  }
+
+  static async findAll(id) {
+    return await Transfers.findAll({
+      where: {
+        senderUserId: id,
+      },
+    })
+  }
+}
---
+++ src/modules/users/user.controller.js
@@ -0,0 +1,74 @@
+import { TransferService } from '../transfers/transfers.service.js'
+import { UserService } from './user.service.js'
+
+export const signup = async (req, res) => {
+  try {
+    const { name, password } = req.body
+    let accountNumber = Math.floor(Math.random() * 99999999)
+
+    const user = await UserService.create({
+      name,
+      password,
+      accountNumber,
+    })
+
+    return res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal serrver error',
+      error,
+    })
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const { accountNumber, password } = req.body
+
+    const user = await UserService.login({ accountNumber, password })
+
+    if (!user) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'User not found',
+      })
+    }
+
+    return res.status(200).json({
+      message: 'You are logged',
+      user,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal serrver error',
+      error,
+    });
+  }
+}
+
+export const getHistory = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const transferHistoryById = await TransferService.findAll(id)
+    if (!transferHistoryById) {
+      return res.status(400).json({
+        status: 'error',
+        message: `Transfer of user by id ${id} not exist`,
+      })
+    }
+
+    return res.status(200).json(transferHistoryById)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal serrver error',
+      error,
+    })
+  }
+}
